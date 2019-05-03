a2c2d61c9204f5c886c1e27ebdd7495f1e4c3408
Julian Gallegos <julianga@uw.edu>
2019-05-02 18:13:27 -0700

    HW5.1 Graph.java and Node.java Specs done?

--- src/main/java/graph/Graph.java
+++ src/main/java/graph/Graph.java
@@ -6,29 +6,93 @@ public class Graph {
 
     private List<Node> graph;
 
+    /** Creates a new Graph with an empty graph list.
+     * @spec.effects Constructs a new Graph g with g.graph.isEmpty() = true.
+     */
     public Graph() { }
 
+    /** Creates a new Graph with one node in the graph list.
+     * @param node - The name of the Node being added to Graph.
+     * @spec.requires Name of "node" is at least one character.
+     * @spec.effects Constructs a new Graph g with Node "node" in g.graph.
+     */
     public Graph(String node) { }
 
+    /** Adds a new node to this Graph.
+     * @spec.requires node has a unique name
+     *                node character length > 0.
+     * @spec.modifies this.graph
+     * @spec.effects Adds a new Node "node" to this.graph.
+     */
     public void addNode() {
         throw new RuntimeException("Graph.addNode() is not yet implemented");
     }
 
-    public void addEdge(Node a, Node b, String edge) {
+    /** Adds a new edge from one Node to another Node in Graph.
+     * @param parent - Name of parent Node for the new edge.
+     * @param child - Name of child Node for the new edge.
+     * @param edge - Label for the new edge.
+     * @spec.requires Node's a and b both exist
+     *                an identical edge with a same label from Node a to Node b does not exist.
+     * @spec.modifies this.graph
+     * @spec.effects Add an edge to Node a that travels to the child Node b.
+     */
+    public void addEdge(String parent, String child, String edge) {
         throw new RuntimeException("Graph.addEdge() is not yet implemented");
     }
 
+    /** Removes a node from the Graph.
+     * @param name - The name of the node to remove.
+     * @spec.requires node exists.
+     * @spec.modifies this.graph
+     * @spec.effects Removes the Node with name "name" from this.graph, as well as
+     *               all of the edges going to and from it.
+     */
+    public void removeNode(String name) {
+        throw new RuntimeException("Graph.removeNode() is not yet implemented");
+    }
+
+    /** Removes an edge from the Graph.
+     * @param parent - Name of the parent node that the edge is coming from.
+     * @param child - Name of the child node that the edge is going to.
+     * @param edge  - Label of the edge going from parent to child.
+     * @spec.requires parent, child, and edge exists.
+     * @spec.modifies this.graph
+     * @spec.effects Removes the edge with label "edge" coming from parent
+     *               to child in this.graph.
+     */
+    public void removeEdge(String parent, String child, String edge) {
+        throw new RuntimeException("Graph.removeEdge() is not yet implemented");
+    }
+
+    /** Print a list of all Node a's child Nodes to console.
+     * @param a - Parent Node to get list of children from.
+     * @spec.requires Node a exist.
+     */
     public void listChildren(Node a) {
         throw new RuntimeException("Graph.listChildren() is not yet implemented");
     }
 
+    /** Print a list of all of the out edges from a Node to console.
+     * @param a - Parent Node to get list of out edges from.
+     * @spec.requires Node a exist.
+     */
     public void listOutEdges(Node a) {
         throw new RuntimeException("Graph.listOutEdges() is not yet implemented");
     }
 
+    /** Print a list of all the Nodes in this Graph.
+     */
     public void listNodes() {
         throw new RuntimeException("Graph.listNodes() is not yet implemented");
     }
 
+    /** Removes all Nodes and edges from the Graph.
+     * @spec.modifies this.graph
+     * @spec.effects Removes all Nodes and edges from this.graph.
+     */
+    public void clear() {
+        throw new RuntimeException("Graph.clear() is not yet implemented");
+    }
 
 }
--- src/main/java/graph/Node.java
+++ src/main/java/graph/Node.java
@@ -8,19 +8,35 @@ public class Node {
     private String name;
     private Map<Node, Set<String>> list;
 
-    public Node() { }
+    /** Creates a new Node.
+     * @param a Name of the new Node.
+     * @spec.requires String a.length > 0.
+     * @spec.effects Constructs a new Node n with n.name = String a.
+     */
+    public Node(String a) { }
 
-    public Node(Node a, String edge) { }
-
-    public Set<String> getEdge(Node a) {
+    /** Returns a copy of Node's set of edges.
+     * @spec.requires Node.list.isEmpty() = false.
+     * @return this.list
+     */
+    public Set<String> getEdges() {
         throw new RuntimeException("Node.getEdge() is not yet implemented");
     }
 
+    /** Returns the name of this Node.
+     * @return this.name
+     */
     public String getName() {
         throw new RuntimeException("Node.getName() is not yet implemented");
     }
 
-    public void addEdge(String edge) {
+    /** Adds an edge from this Node to a child Node.
+     * @spec.requires child exists
+     * @spec.modifies this.list
+     * @spec.effects If Node not in this.list, adds it as a key and the String representation of a edge to its value set..
+     *               Otherwise adds the String edge to the value set for Node child in this.list
+     */
+    public void addEdge(String child, String edge) {
         throw new RuntimeException("Node.addEdge() is not yet implemented");
     }
 
