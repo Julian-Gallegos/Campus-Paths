8d987778ebefaecae23eec3bbdfb9d76827298ae
Julian Gallegos <julianga@uw.edu>
2019-05-22 23:51:15 -0700

    Created dickstraws class

---
+++ src/main/java/pathfinder/Dijkstra.java
@@ -0,0 +1,16 @@
+package pathfinder;
+
+import graph.Graph;
+
+import java.util.PriorityQueue;
+
+public class Dijkstra<E> {
+
+    // Not an ADT
+
+    public void search(E starting_node, E destination_node, Graph<E,Double> graph) {
+
+        PriorityQueue<E> active = new PriorityQueue<>();
+        
+    }
+}
