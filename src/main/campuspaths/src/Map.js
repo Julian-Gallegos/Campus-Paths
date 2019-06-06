a7c332ac99f9e95476c91242c0aa06370d471d45
Julian Gallegos <julianga@uw.edu>
2019-06-06 15:58:02 -0700

    who needs maps anyway

--- src/main/campuspaths/src/Map.js
+++ src/main/campuspaths/src/Map.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import "./Map.css";
+import Button from "./Button";
 
 class Map extends Component {
 
@@ -12,21 +13,60 @@ class Map extends Component {
 
   constructor(props) {
     super(props);
+    this.canvasReference = React.createRef();
     this.backgroundImage = new Image();
     this.backgroundImage.onload = () => {
-      // TODO: Do something when the image is ready?
+      this.drawBackgroundImage();
     };
-    this.backgroundImage.src = ""; // TODO: Fill this in.
+    this.backgroundImage.src = "./campus_map.jpg";
   }
 
   drawBackgroundImage() {
-    let canvas = null; // TODO Fill this in with the canvas, not the context.
+    let canvas = this.canvasReference.current;
     let ctx = canvas.getContext("2d");
-    //
     if (this.backgroundImage.complete) { // This means the image has been loaded.
       canvas.width = this.backgroundImage.width;
       canvas.height = this.backgroundImage.height;
       ctx.drawImage(this.backgroundImage, 0, 0);
+    }
+  }
+
+  drawLines(path) {
+    let canvas = this.canvasReference.current;
+    let ctx = canvas.getContext("2d");
+    let pathCollection = path.path;
+    this.drawBackgroundImage();
+
+    pathCollection.forEach((element) => {
+      let x1 = element.start.x;
+      let y1 = element.start.y;
+      let x2 = element.end.x;
+      let y2 = element.end.y;
+      ctx.beginPath();
+      ctx.lineWidth = 25;
+      ctx.moveTo(x1, y1);
+      ctx.lineTo(x2, y2);
+      ctx.strokeStyle = "red";
+      ctx.lineCap = "round";
+      ctx.stroke();
+    });
+  }
+
+  findPathData() {
+    if(this.props.source === "" || this.props.destination === "") {
+      alert("Must Select a building for both start and destination");
+    } else {
+      fetch("http://localhost:4567/find-path?start=" + this.props.source + "&end=" + this.props.destination).then(
+          (res) => {
+            if (res.status !== 200) {
+              throw Error("Response doesn't have 200 - OK status.")
+            }
+            return res.json();
+          }
+      ).then((resText) => {
+            this.drawLines(resText);
+          }
+      );
     }
   }
 
@@ -37,6 +77,21 @@ class Map extends Component {
     // can get the canvas object and call methods on it.
     return (
       <div className="canvasHolder">
+        <canvas ref={this.canvasReference} />
+
+        <div className="buttons">
+          <div className="innerButton">
+          <Button color="primary" onClick={() => {  // Search path finder
+            this.findPathData();
+          }} value="Search" />
+          </div>
+          <div className="innerButton">
+          <Button color="secondary" onClick={() => {  // clear
+            this.drawBackgroundImage();
+            this.props.onClick();
+          }} value="Clear" />
+          </div>
+        </div>
 
       </div>
     )
